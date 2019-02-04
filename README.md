f6057372eafddb921f189ad6d6bc83f74aad8718
Kevin Salcedo <kevsalcedo@gmail.com>
2019-02-04 08:36:02 -0600

    starting section 7

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# react-course
+# Udemy Course: Modern React and Redux
+As a slight motivator to learn a little more advanced React intricacies, as well as proper convention, I decided to run through a 24 module course covering a large amount of React and Redux. Listed below are different small projects created throughout this online course at [Udemy][https://www.udemy.com/react-redux/].
 
 ## components 
 testing React components
@@ -13,9 +14,16 @@ very simple JSX
 
 * textbox plus label
 
-----
+---
 
 ## seasons
 * testing props, state, and class based components
 * uses async geolocation to get user's latitude
 * conditional rendering of different JSX elements 
+* uses Semantic UI for template icons
+
+---
+
+## pics
+* first version is to search any term and return image results matching term
+* async external api calls
--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,34 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
-const SeasonDisplay = () => {
-    return <div>Season Display</div>;
-}
+const seasonConfig = {
+    summer: {
+        text: 'Lets hit the beach',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Brr, it is cold',
+        iconName: 'snowflake'
+    }
+};
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+};
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const {text, iconName} = seasonConfig[season];
+
+    return <div className={`season-display ${season}`}>
+        <i className={`${iconName} icon-left massive icon`} />
+        <h1>{text}</h1>
+        <i className={`${iconName} icon-right massive icon`} />
+    </div>
+};
 
 export default SeasonDisplay;
