b7ff718e8cb0461246d7717e8193f6860a07a722
Kevin Salcedo <kevsalcedo@gmail.com>
2019-02-04 09:59:11 -0600

    starting section 8 - api requests

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,18 +1,27 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = {
+        term: ''
+    }
 
-    onInputChange(event) {
-        
+    onFormSubmit = event => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term);
     }
 
     render() {
         return (
         <div className='ui segment'>
-            <form className='ui form'>
+            <form onSubmit={this.onFormSubmit} className='ui form'>
                 <div className='field'>
                     <label>Image Search</label>
-                    <input type='text' onChange={this.onInputChange}/>
+                    <input 
+                        type='text' 
+                        value={this.state.term} 
+                        onChange={(e) => this.setState({term: e.target.value})} 
+                    />
                 </div>
             </form>
         </div>
