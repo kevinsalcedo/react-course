0fdb779391c45d6ee6192cb945bab3d5aa7bc280
Kevin Salcedo <kevsalcedo@gmail.com>
2019-02-27 20:25:11 -0600

    pc switch

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -13,7 +13,6 @@ class GoogleAuth extends React.Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
-
           this.onAuthChange(this.auth.isSignedIn.get());
           this.auth.isSignedIn.listen(this.onAuthChange);
         });
@@ -30,6 +29,7 @@ class GoogleAuth extends React.Component {
 
   onSignInClick = () => {
     this.auth.signIn();
+    this.auth.getAuthInstance().getUser();
   };
 
   onSignOutClick = () => {
